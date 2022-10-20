ee54195ceab68550d58a8c3a08ba58f18edb221e
missozoda <ozoshka1207@gmail.com>
2022-10-20 14:58:38 +0500

    add responsive to Ads section

--- src/components/Ads.vue
+++ src/components/Ads.vue
@@ -1,12 +1,12 @@
 <template>
   <section class="ads py-24 mb-20">
     <Container>
-      <div class="flex justify-between items-center text-white">
-        <div class="w-2/3">
-          <h3 class="font-bold text-4xl mb-8">Free consultation with exceptional quality</h3>
-          <p class="text-2xl">Just one call away: +84 1102 2703</p>
+      <div class="flex lg:flex-row flex-col justify-between items-center text-white">
+        <div class="lg:w-2/3 w-full mb-8 lg:mb-0">
+          <h3 class="font-bold md:text-4xl text-2xl mb-8">Free consultation with exceptional quality</h3>
+          <p class="md:text-2xl text-xl">Just one call away: +84 1102 2703</p>
         </div>
-        <div class="w-1/3">
+        <div class="lg:w-1/3 w-full">
           <button class="border-2 border-white py-3 px-5 text-lg"> Get your consultation </button>
         </div>
       </div>
